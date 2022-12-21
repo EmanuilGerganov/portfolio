d6d40a356db7856c3528d1491443799a31e04bd7
Emo <emanuilgerganov@gmail.com>
2022-12-21 23:05:29 +0200

    add router base

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@
 export default defineNuxtConfig({
   modules: ["@vueuse/nuxt", "@nuxt/image-edge"],
   ssr: false,
-
+  router: {
+    base: "/portfolio/",
+  },
   css: ["~/assets/main.css"],
   postcss: {
     plugins: {
