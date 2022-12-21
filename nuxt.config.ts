262d4d41e46f95f23d5c02212d797ae76ab728d3
Emo <emanuilgerganov@gmail.com>
2022-12-21 22:56:08 +0200

    push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["@vueuse/nuxt", "@nuxt/image-edge"],
+  ssr: false,
+
   css: ["~/assets/main.css"],
   postcss: {
     plugins: {
