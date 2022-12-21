b660360853155fdb93246236f8dd1c18eedfd570
Emo <emanuilgerganov@gmail.com>
2022-12-21 23:23:52 +0200

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@
 export default defineNuxtConfig({
   modules: ["@vueuse/nuxt", "@nuxt/image-edge"],
   ssr: false,
-  router: {
-    base: "/portfolio/",
-  },
   css: ["~/assets/main.css"],
   postcss: {
     plugins: {
